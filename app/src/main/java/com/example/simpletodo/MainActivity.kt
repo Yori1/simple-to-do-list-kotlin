edce9becd1ec443bd32bf01cbaff27ba4fd4d8db
Yori Garcia Rosario <yorigr@outlook.com>
2019-09-08 22:58:26 +0200

    fixed issue when typing, overlapping elements

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -8,7 +8,6 @@ import com.example.simpletodo.logic.TaskListRevealer
 import com.example.simpletodo.logic.adapters.ClearRecyclerAdapter
 import com.example.simpletodo.logic.adapters.TaskRecyclerAdapter
 import com.example.simpletodo.models.ToDoTask
-import android.R
 import android.view.View
 import android.widget.Button
 
@@ -24,8 +23,6 @@ class MainActivity : AppCompatActivity() {
         prepareTaskRecyclerView()
         prepareClearRecyclerView()
 
-        val buttonAll: Button = findViewById(R.id.button_all)
-        mClickButton1.setOnClickListener(this)
 
         taskListRevealer.addTask(ToDoTask("task", false))
         taskListRevealer.addTask(ToDoTask("task", false))
@@ -36,11 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     fun onClick(v: View) {
         when (v.getId()) {
-            R.id.clickButton1 -> {
-            }// do something for button 1 click
 
-            R.id.clickButton2 -> {
-            }// do something for button 2 click
         }//.... etc
     }
 
